72268c8f3f354423c12a1ad97cd3f0594fbc7adf
ZoDream <648383079@qq.com>
2019-07-01 23:00:19 +0800

    修改

--- zodream-cocos/assets/Script/Helloworld.ts
+++ zodream-cocos/assets/Script/Helloworld.ts
@@ -6,11 +6,22 @@ export default class Helloworld extends cc.Component {
     @property(cc.Label)
     label: cc.Label = null;
 
+    @property(cc.Button)
+    loginBtn: cc.Button = null;
+
     @property
     text: string = 'hello';
 
+    onLoad() {
+        this.loginBtn.node.on('click', this.login, this);
+    }
+
     start () {
         // init logic
-        this.label.string = this.text;
+        //this.label.string = this.text;
+    }
+
+    login() {
+        cc.director.loadScene('play');
     }
 }
